3647ddbdde0e03927c6fd334be0c67d47989d23c
Carlos Aruh <carlos.aruh@gmail.com>
2024-06-13 16:56:10 -0300

    Final

---
+++ src/Funcionalidades/Navegador/NavegadorInternet.java
@@ -0,0 +1,11 @@
+package Funcionalidades.Navegador;
+
+public interface NavegadorInternet {
+
+	public void pagina();
+
+	public void adicionandoNovaPagina();
+
+	public void atualizaPagina();
+
+}
---
+++ src/Funcionalidades/Telefone/AparelhoTelefonico.java
@@ -0,0 +1,10 @@
+package Funcionalidades.Telefone;
+
+public interface AparelhoTelefonico {
+
+	public void ligar();
+
+	public void atender();
+
+	public void iniciarCorreioVoz();
+}
